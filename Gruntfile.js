89ae64d77f9633f018bb947ba6a01ca661e0a32f
toihocweb <buidinhnhat2030@gmail.com>
2019-03-11 08:48:26 +0700

    a

--- Gruntfile.js
+++ Gruntfile.js
@@ -133,7 +133,7 @@ module.exports = function(grunt) {
 				tasks: 'css-themes'
 			},
 			css: {
-				files: [ 'css/reveal.scss' ],
+				files: [ 'css/reveal.scss'],
 				tasks: 'css-core'
 			},
 			html: {
